d03e831cf41a70a2241ab6c04e9c99866d83bebf
oleg_m <medinschi.oleg@gmail.com>
2024-05-30 17:18:24 +0300

    PostgresSQL data base connected & full text search

--- etehno/settings.py
+++ etehno/settings.py
@@ -29,7 +29,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
+
     "debug_toolbar",
+    
     "main",
     "goods", 
 ]
@@ -71,10 +74,22 @@ WSGI_APPLICATION = "etehno.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "etehno",
+        "USER": "etehno",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
